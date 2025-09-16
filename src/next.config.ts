7afac8ceb3dc871e6e6d7d50d1ced555b9d424af
KC Yim <dreailsong@gmail.com>
2025-09-16 15:34:19 +0000

    都仲係internal server error🥹

---
+++ src/next.config.ts
@@ -0,0 +1,44 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // This is to allow 'firebase/messaging/sw' to be imported in the service worker.
+    if (!isServer) {
+        config.resolve.alias['firebase/messaging/sw'] = 'firebase/messaging/sw';
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
