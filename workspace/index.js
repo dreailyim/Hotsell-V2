880eca9bd6ee8ce9dcb339704c9884f88867b43c
KC Yim <dreailsong@gmail.com>
2025-09-16 08:09:49 +0000

    點解deploy 完個web app internal server error

--- workspace/index.js
+++ workspace/index.js
@@ -3,7 +3,10 @@ const functions = require("firebase-functions");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({ dev, conf: { distDir: ".next" } });
+// The `distDir` is relative to the `index.js` file.
+// Since `index.js` is in the `workspace` directory, we need to go up one level
+// to find the `.next` directory, which is in the root.
+const nextApp = next({ dev, conf: { distDir: "../.next" } });
 const handle = nextApp.getRequestHandler();
 
 // Main web API that serves the Next.js app
