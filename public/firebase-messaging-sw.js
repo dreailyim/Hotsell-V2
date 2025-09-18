b1b40ff493d0b37957adfef3689c02d798490594
KC Yim <dreailsong@gmail.com>
2025-09-18 23:44:02 +0000

    ?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,37 +1,33 @@
-// This service worker can be customized!
-// See https://firebase.google.com/docs/messaging/js/receive
+// Import the Firebase app and messaging packages using importScripts
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
 
-// Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed
-import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging/sw';
-
-// This configuration is safe to be exposed on the client-side.
 const firebaseConfig = {
-  "projectId": "hotsell-dolw2",
-  "appId": "1:25821240563:web:0c84f1a6f053f3e9e12b86",
-  "storageBucket": "hotsell-dolw2.appspot.com",
-  "apiKey": "AIzaSyAZChqV6v73lcJBCMVXIdd4VlREq7tdDVo",
-  "authDomain": "hotsell-dolw2.firebaseapp.com",
-  "messagingSenderId": "25821240563",
-  "measurementId": "G-5G6503TB6P",
-  "databaseURL": "https://hotsell-dolw2.firebaseio.com"
+  apiKey: "AIzaSyAZChqV6v73lcJBCMVXIdd4VlREq7tdDVo",
+  authDomain: "hotsell-dolw2.firebaseapp.com",
+  projectId: "hotsell-dolw2",
+  storageBucket: "hotsell-dolw2.appspot.com",
+  messagingSenderId: "25821240563",
+  appId: "1:25821240563:web:0c84f1a6f053f3e9e12b86",
+  measurementId: "G-5G6503TB6P",
+  databaseURL: "https://hotsell-dolw2.firebaseio.com"
 };
 
-const app = initializeApp(firebaseConfig);
-const messaging = getMessaging(app);
+// Initialize the Firebase app in the service worker
+firebase.initializeApp(firebaseConfig);
 
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-//
-// self.addEventListener('push', function(event) {
-//   console.log('[firebase-messaging-sw.js] Received push event.', event);
+// Retrieve an instance of Firebase Messaging
+const messaging = firebase.messaging();
+
+// If you want to handle background messages, you can add a handler here.
+// messaging.onBackgroundMessage((payload) => {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
 //   // Customize notification here
-//   const notificationTitle = 'New Message';
+//   const notificationTitle = payload.notification.title;
 //   const notificationOptions = {
-//     body: 'You have a new message.',
+//     body: payload.notification.body,
 //     icon: '/firebase-logo.png'
 //   };
-//   event.waitUntil(self.registration.showNotification(notificationTitle, notificationOptions));
+//
+//   self.registration.showNotification(notificationTitle, notificationOptions);
 // });
