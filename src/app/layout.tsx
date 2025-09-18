a6a2c817b73e711271634f6468cae2cea5234786
KC Yim <dreailsong@gmail.com>
2025-09-18 02:53:24 +0000

    OK，而家搞個adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "HotSell",
@@ -17,6 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6189242189989239"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
       </head>
       <body className="font-body antialiased" suppressHydrationWarning>
         <Providers>
