afa7bf2626323fc6bdd8d3034c1783fbe08c05c0
KC Yim <dreailsong@gmail.com>
2025-09-26 10:31:12 +0000

    係咪卡住咗？好耐了

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // This allows the Next.js dev server to accept requests from the
+    // Firebase Studio development preview URL.
+    allowedDevOrigins: [
+      '9003-firebase-studio-1756768466557.cluster-ubrd2huk7jh6otbgyei4h62ope.cloudworkstations.dev',
+    ],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
