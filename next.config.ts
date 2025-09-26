26b65327244ea9eaaa6f45153b3baeb370db3195
KC Yim <dreailsong@gmail.com>
2025-09-26 09:56:00 +0000

    搞唔掂喎，你頭先做過啲咩，不如你復完返先

--- next.config.ts
+++ next.config.ts
@@ -23,12 +23,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
     ],
   },
 };
