52fc563e15b45fade6043d5e3107bfb372c3b7d0
KC Yim <dreailsong@gmail.com>
2025-09-30 00:42:10 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,36 @@
+import withPWA from '@ducanh2912/next-pwa';
 
-/** @type {import('next').NextConfig} */
-import createNextPwa from '@ducanh2912/next-pwa';
-
-const withPWA = createNextPwa({
+const pwa = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config options here
-  webpack: (config, { isServer, dev }) => {
-    // Exclude the custom server sw from the main app builds
-    if (!isServer && !dev) {
-        config.externals = {
-            ...config.externals,
-            './firebase/messaging-sw': 'self.firebase-messaging-sw.js',
-        };
-    }
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false, // This is to fix 'fs' module not found error in browser
-    };
-    return config;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
-export default withPWA(nextConfig);
+export default pwa(nextConfig);
