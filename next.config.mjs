3d9d393b570d7d2bcdfeeee72d2003c02f01c067
KC Yim <dreailsong@gmail.com>
2025-10-01 01:19:27 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,63 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import WithPWA from '@ducanh2912/next-pwa';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 /** @type {import('next').NextConfig} */
+import withPWA from '@ducanh2912/next-pwa';
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  // Other PWA options
+});
+
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // 在這裡設定 Service Worker 的進入點
-      config.entry['firebase-messaging-sw'] = path.join(
-        __dirname,
-        'src',
-        'lib',
-        'firebase',
-        'messaging-sw.ts'
-      );
+  // Your regular Next.js config
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  webpack: (config, { isServer, dev }) => {
+    if (isServer) {
+      // (server-side-only compilation)
     }
+
+    if (!isServer) {
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            //This is required for genkit to work.
+            "http": false,
+            "https": false,
+            "url": false,
+            "zlib": false,
+            "net": false,
+            "tls": false,
+            "fs": false,
+        };
+    }
+    
+    // Compile the service worker
+    if (!dev && !isServer) {
+        config.entry.sw = ['./src/lib/firebase/messaging-sw.ts'];
+    }
+
     return config;
   },
 };
 
-export default WithPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  // runtimeCaching: [], // 您可以根據需要自訂快取策略
-})(nextConfig);
+export default pwaConfig(nextConfig);
