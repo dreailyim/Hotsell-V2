a1c6fe146412a7957fa1016b4e174dc50da6c9a9
KC Yim <dreailsong@gmail.com>
2025-10-01 01:17:20 +0000

    ?

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,31 @@
-import withPWA from '@ducanh2912/next-pwa';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import WithPWA from '@ducanh2912/next-pwa';
 
-const pwa = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // 在這裡設定 Service Worker 的進入點
+      config.entry['firebase-messaging-sw'] = path.join(
+        __dirname,
+        'src',
+        'lib',
+        'firebase',
+        'messaging-sw.ts'
+      );
+    }
+    return config;
   },
 };
 
-export default pwa(nextConfig);
+export default WithPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  // runtimeCaching: [], // 您可以根據需要自訂快取策略
+})(nextConfig);
