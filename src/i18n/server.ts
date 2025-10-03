5e516d9bacc8cd30a0044c6e47935db6f9bcc4bc
KC Yim <dreailsong@gmail.com>
2025-10-03 03:32:02 +0000

    server.ts 檔案檢查
    這裡很可能就是問題所在。
    createI18nServer 的設定使用了 () => import('./en'

--- src/i18n/server.ts
+++ src/i18n/server.ts
@@ -1,6 +1,6 @@
 import { createI18nServer } from 'next-international/server';
 
 export const { getI18n, getScopedI18n, getStaticParams, getCurrentLocale } = createI18nServer({
-  en: () => import('./en'),
-  zh: () => import('./zh'),
+  en: () => import('./en').then(m => m.default),
+  zh: () => import('./zh').then(m => m.default),
 });
