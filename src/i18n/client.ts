a88acda91ae7eb0d3fd0dcb7bf80373f2f632348
KC Yim <dreailsong@gmail.com>
2025-10-04 07:23:45 +0000

    無就繼續其他

--- src/i18n/client.ts
+++ src/i18n/client.ts
@@ -1,8 +1,6 @@
 'use client';
 
 import { createI18nClient } from 'next-international/client';
-import { en } from './en';
-import { zh } from './zh';
 
 export const { useI18n, useScopedI18n, I18nProviderClient, useChangeLocale, useCurrentLocale } = createI18nClient({
   en: () => import('./en'),
--- src/i18n/server.ts
+++ src/i18n/server.ts
@@ -1,6 +1,4 @@
 import { createI18nServer } from 'next-international/server';
-import { en } from './en';
-import { zh } from './zh';
 
 export const { getI18n, getScopedI18n, getStaticParams, getCurrentLocale } = createI18nServer({
   en: () => import('./en'),
